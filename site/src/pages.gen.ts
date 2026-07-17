f1dc0d1df58912f2f2df82ff193578d17f1f896c
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2026-07-17 16:18:19 +0200

    docs: split sidebar sections into pages

--- site/src/pages.gen.ts
+++ site/src/pages.gen.ts
@@ -5,7 +5,18 @@ import type { PathsForPages } from 'waku/router'
 
 // prettier-ignore
 type Page =
+  | { path: '/docs/cli'; render: 'static' }
+  | { path: '/docs/devtools'; render: 'static' }
+  | { path: '/docs/hot-reload'; render: 'static' }
+  | { path: '/docs/how-it-works'; render: 'static' }
   | { path: '/docs'; render: 'static' }
+  | { path: '/docs/installation'; render: 'static' }
+  | { path: '/docs/plugin-api'; render: 'static' }
+  | { path: '/docs/quick-start'; render: 'static' }
+  | { path: '/docs/security'; render: 'static' }
+  | { path: '/docs/troubleshooting'; render: 'static' }
+  | { path: '/docs/ui-contributions'; render: 'static' }
+  | { path: '/docs/your-first-plugin'; render: 'static' }
   | { path: '/'; render: 'static' }
 
 // prettier-ignore
